8d2a021469e24e18360d6ff881d1ebce392b7065
Bruno Zaccariello <30813635+SpaceTheArcher@users.noreply.github.com>
2017-11-08 15:12:50 -0200

    v0.0.1

--- app/views.py
+++ app/views.py
@@ -1,59 +1,49 @@
+"""
+Definition of views.
+"""
+
 from django.shortcuts import render
-from django.http import request
-# Create your views here.
+from django.http import HttpRequest
+from django.template import RequestContext
+from datetime import datetime
 
-def index(request):
-    return render(request, "index.html")
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Home Page',
+            'year':datetime.now().year,
+        })
+    )
 
-def page_lista_cursos(request):
-    contexto = {
-        "cursos":[
-        {"nome":"Segurança Da Informação","link":"/Disciplinas/SegDaInformacao"}, 
-        {"nome":"Arquitetura de Software","link":"PADS"}, 
-        {"nome":"Jogos Digitais","link":"JD"},
-        {"nome":"Redes de Computadores","link":"RC"},
-        {"nome":"Sistemas de Informação","link":"SI"}
-        ],
-        "faculdade":"Faculdade Lorenzinni",
-    }
-    return render(request, "ListaCursos.html", contexto)
+def contact(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/contact.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Contact',
+            'message':'Your contact page.',
+            'year':datetime.now().year,
+        })
+    )
 
-def page_noticias(request):
-    return render(request, "noticias.html")
-
-def page_login(request):
-    return render(request, "LoginPage.html")
-
-def page_nova_senha(request):
-    return render(request, "ForgotPass.html")
-
-def page_cadastro_disciplina(request):
-    return render(request, "CadastrarDisciplina.html")
-
-def page_contato(request):
-    if request.POST:
-        form = ContatoForm(request.POST)
-        if form.is_valid() :
-            form.envia_email()
-    else:
-        form = CursoForm()
-        print('Executei um GET')
-    contexto = {
-        "form":form
-    }
-    return render(request, "Contato.html", contexto)
-
-def page_cadastro_usuario(request):
-    return render(request, "CadastroPage.html")
-
-def page_detalhes_cursos(request):
-    return render(request, "DesCurso.html")
-
-def page_detalhes_segdainf(request):
-    return render(request, "SegDaInf.html")
-
-def page_disciplinas_segdainf(request):
-    return render(request, "dSegDaInfo.html")
-
-def page_disciplina_seginfoatualidade(request):
-    return render(request, "SegInfoAtualidade.html")
+def about(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/about.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'About',
+            'message':'Your application description page.',
+            'year':datetime.now().year,
+        })
+    )
