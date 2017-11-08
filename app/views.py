c900b91490562395a2b9eee27426e9cc54e88a8b
Bruno Zaccariello <30813635+SpaceTheArcher@users.noreply.github.com>
2017-11-08 16:25:27 -0200

    v1.0.1

--- app/views.py
+++ app/views.py
@@ -1,60 +1,150 @@
 from django.shortcuts import render
 from django.http import request
-from core.forms import ContatoForm
+from django.template import RequestContext
+from datetime import datetime
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html")
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Home Page',
+            'year':datetime.now().year,
+        })
+    )
 
 def page_lista_cursos(request):
-    contexto = {
-        "cursos":[
-        {"nome":"Segurança Da Informação","link":"/Disciplinas/SegDaInformacao"}, 
-        {"nome":"Arquitetura de Software","link":"PADS"}, 
-        {"nome":"Jogos Digitais","link":"JD"},
-        {"nome":"Redes de Computadores","link":"RC"},
-        {"nome":"Sistemas de Informação","link":"SI"}
-        ],
-        "faculdade":"Faculdade Lorenzinni",
-    }
-    return render(request, "ListaCursos.html", contexto)
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/ListaCursos.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Lista de Cursos',
+            'year':datetime.now().year,
+            'cursos':{{'nome':'SI', 'link':''}}
+        })
+    )
 
 def page_noticias(request):
-    return render(request, "noticias.html")
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/noticias.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Noticias',
+            'year':datetime.now().year,
+        })
+    )
 
 def page_login(request):
-    return render(request, "LoginPage.html")
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/LoginPage.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Página de Login',
+            'year':datetime.now().year,
+        })
+    )
 
 def page_nova_senha(request):
-    return render(request, "ForgotPass.html")
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/ForgotPass.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Esqueci Minha Senha',
+            'year':datetime.now().year,
+        })
+    )
 
 def page_cadastro_disciplina(request):
-    return render(request, "CadastrarDisciplina.html")
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/CadastroDisciplina.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Cadastre uma Disciplina',
+            'year':datetime.now().year,
+        })
+    ))
 
 def page_contato(request):
-    if request.POST:
-        form = ContatoForm(request.POST)
-        if form.is_valid() :
-            form.envia_email()
-    else:
-        form = CursoForm()
-        print('Executei um GET')
-    contexto = {
-        "form":form
-    }
-    return render(request, "Contato.html", contexto)
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/Contato.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Contato',
+            'year':datetime.now().year,
+        })
+    )
 
 def page_cadastro_usuario(request):
-    return render(request, "CadastroPage.html")
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/CadastroPage.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Cadastre-se',
+            'year':datetime.now().year,
+        })
+    )
 
 def page_detalhes_cursos(request):
-    return render(request, "DesCurso.html")
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/DesCurso.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Detalhes dos Cursos',
+            'year':datetime.now().year,
+        })
+    )
 
 def page_detalhes_segdainf(request):
-    return render(request, "SegDaInf.html")
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/SegDaInf.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Detalhes - Seg. Da Informação',
+            'year':datetime.now().year,
+        })
+    )
 
 def page_disciplinas_segdainf(request):
-    return render(request, "dSegDaInfo.html")
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/dSegDaInfo.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Disciplinas Seg. Da Informação',
+            'year':datetime.now().year,
+        })
+    )
 
 def page_disciplina_seginfoatualidade(request):
-    return render(request, "SegInfoAtualidade.html")
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/SegInfoAtualidade.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Seg. Da Informação na Atualidade',
+            'year':datetime.now().year,
+        })
+    )
