9714b12f7f6abc8fe43dfda5320cf4670cba19fe
Bruno Zaccariello <30813635+SpaceTheArcher@users.noreply.github.com>
2017-11-08 16:10:12 -0200

    v1.0.0 (Beta)

--- app/models.py
+++ app/models.py
@@ -1,7 +1,10 @@
-"""
-Definition of models.
-"""
-
 from django.db import models
 
 # Create your models here.
+
+class Curso(models.Model):
+    
+    carga_horaria = models.IntegerField("Carga Horária")
+    nome = models.CharField("Nome",max_length = 20)
+    tipo = models.CharField("Tipo",max_length = 20)
+    professor = models.CharField("Professor",max_length=50)
--- app/views.py
+++ app/views.py
@@ -1,49 +1,127 @@
-"""
-Definition of views.
-"""
-
 from django.shortcuts import render
-from django.http import HttpRequest
+from django.http import request
 from django.template import RequestContext
 from datetime import datetime
+from app.models import Curso
+# Create your views here.
 
-def home(request):
-    """Renders the home page."""
+def index(request):
     assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/index.html',
-        context_instance = RequestContext(request,
-        {
-            'title':'Home Page',
-            'year':datetime.now().year,
-        })
-    )
+    return render(request, "app/index.html",
+                  context_instance = RequestContext(request,
+                  {
+                      'title':'Home Page',
+                      'year':datetime.now().year,
+                  })
+                  )
 
-def contact(request):
-    """Renders the contact page."""
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/contact.html',
-        context_instance = RequestContext(request,
-        {
-            'title':'Contact',
-            'message':'Your contact page.',
-            'year':datetime.now().year,
-        })
-    )
+def page_lista_cursos(request):
+        assert isinstance(request, HttpRequest)
+        return render(request, "app/ListaCursos.html",
+                  context_instance = RequestContext(request,
+                  {
+                      'title':'Lista de Cursos',
+                      'year':datetime.now().year,
+                      'cursos':Curso.objects.all()
+                  })
+                  )
 
-def about(request):
-    """Renders the about page."""
+def page_noticias(request):
     assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/about.html',
-        context_instance = RequestContext(request,
-        {
-            'title':'About',
-            'message':'Your application description page.',
-            'year':datetime.now().year,
-        })
-    )
+    return render(request, "app/noticias.html",
+                  context_instance = RequestContext(request,
+                  {
+                      'title':'Noticias',
+                      'year':datetime.now().year,
+                  })
+                  )
+
+def page_login(request):
+    assert isinstance(request, HttpRequest)
+    return render(request, "app/LoginPage.html",
+                  context_instance = RequestContext(request,
+                  {
+                      'title':'Página de Login',
+                      'year':datetime.now().year,
+                  })
+                  )
+
+def page_nova_senha(request):
+    assert isinstance(request, HttpRequest)
+    return render(request, "app/ForgotPass.html",
+                  context_instance = RequestContext(request,
+                  {
+                      'title':'Esqueci Minha Senha',
+                      'year':datetime.now().year,
+                  })
+                  )
+
+def page_cadastro_disciplina(request):
+    assert isinstance(request, HttpRequest)
+    return render(request, "app/CadastrarDisciplina.html",
+                  context_instance = RequestContext(request,
+                  {
+                      'title':'Cadastrar Disciplina',
+                      'year':datetime.now().year,
+                  })
+                  )
+
+def page_contato(request):
+    assert isinstance(request, HttpRequest)
+    return render(request, "app/Contato.html",
+                  context_instance = RequestContext(request,
+                  {
+                      'title':'Contato',
+                      'year':datetime.now().year,
+                  })
+                  )
+
+def page_cadastro_usuario(request):
+    assert isinstance(request, HttpRequest)
+    return render(request, "app/CadastroPage.html",
+                  context_instance = RequestContext(request,
+                  {
+                      'title':'Cadastre-se',
+                      'year':datetime.now().year,
+                  })
+                  )
+
+def page_detalhes_cursos(request):
+    assert isinstance(request, HttpRequest)
+    return render(request, "app/DesCurso.html",
+                  context_instance = RequestContext(request,
+                  {
+                      'title':'Detalhes dos Cursos',
+                      'year':datetime.now().year,
+                  })
+                  )
+
+def page_detalhes_segdainf(request):
+    assert isinstance(request, HttpRequest)
+    return render(request, "app/SegDaInf.html",
+                  context_instance = RequestContext(request,
+                  {
+                      'title':'Detalhes Seg. Da Informação',
+                      'year':datetime.now().year,
+                  })
+                  )
+
+def page_disciplinas_segdainf(request):
+    assert isinstance(request, HttpRequest)
+    return render(request, "app/dSegDaInfo.html",
+                  context_instance = RequestContext(request,
+                  {
+                      'title':'Disciplinas Seg. da Informação',
+                      'year':datetime.now().year,
+                  })
+                  )
+
+def page_disciplina_seginfoatualidade(request):
+    assert isinstance(request, HttpRequest)
+    return render(request, "app/SegInfoAtualidade.html",
+                  context_instance = RequestContext(request,
+                  {
+                      'title':'Segurança da Informação na Atualidade',
+                      'year':datetime.now().year,
+                  })
+                  )
